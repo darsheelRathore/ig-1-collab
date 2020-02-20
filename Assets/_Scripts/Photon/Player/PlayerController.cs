53cdeb8a9d8e9627ad387d02ebbcfa3d9895d435
ibx090 <mailatwork090@gmail.com>
2020-02-20 19:32:52 +0530

    Both player movement bug fixed

--- Assets/_Scripts/Photon/Player/PlayerController.cs
+++ Assets/_Scripts/Photon/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!GetComponent<PhotonView>().IsMine) { return; }
 
         movementVector = transform.forward * Input.GetAxis("Vertical")
             + transform.right * Input.GetAxis("Horizontal");
