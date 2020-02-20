f0812dac003991899de96c8f757a0800f9b8486a
ibx090 <mailatwork090@gmail.com>
2020-02-20 19:27:57 +0530

    Applied movement to the player.

---
+++ Assets/_Scripts/Photon/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController myCC;
+    float inH, inV;
+
+    Vector3 movementVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCC = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movementVector = transform.forward * Input.GetAxis("Vertical")
+            + transform.right * Input.GetAxis("Horizontal");
+
+        movementVector = movementVector.normalized;
+
+        myCC.Move(movementVector * Time.deltaTime * 5f);
+    }
+}
